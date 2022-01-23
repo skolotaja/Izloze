1af761f86d68c0c42084468d089a2d8e2d8dbaf0
skolotaja <94806629+skolotaja@users.noreply.github.com>
2022-01-23 21:23:34 +0200

    cit

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const vardi=['Jana Krūmiņa','Jānis Bērziņš','Dace Priede','Atis Roze','Oto
 const balvas=['Dators','Leduskapis','Tālrunis','Auto','Fotoaparāts'];
 const naudaKopa=100000;//kopeja summa
 let uzvaretajuSkaits=5;
-let rand= Math.random()*5;
+let rand= Math.random()*vardi.length;
 rand= Math.floor(rand);//noapaļo uz leju
 console.log(vardi[rand]);//izvada konsolē
