6bc48cc888a8b5385d3f9c8e6b4b9e26efc20270
skolotaja <maiga.pigita@kkv.lv>
2022-01-24 15:04:02 +0200

    OtraStunda

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const vardi=['Jana Krūmiņa','Jānis Bērziņš','Dace Priede','Atis Roze','Oto
 const balvas=['Dators','Leduskapis','Tālrunis','Auto','Fotoaparāts'];
 const naudaKopa=100000;//kopeja summa
 let uzvaretajuSkaits=5;
-let rand= Math.random()*5;
+let rindas= document.querySelector('.rindas');
+rindas.innerHTML=' ';
+for(let i=0;i<uzvaretajuSkaits;i++){
+let rand= Math.random()*vardi.length;
 rand= Math.floor(rand);//noapaļo uz leju
 console.log(vardi[rand]);//izvada konsolē
+}
+rindas.innerHTML=
+<tr>
+    <td>i</td>
+    <td>uzvarētājs</td>
+</tr>
