7305aa99902159268b9f1486b43aede79cf56d4d
skolotaja <94806629+skolotaja@users.noreply.github.com>
2022-02-02 21:55:34 +0200

    balvas1

--- script.js
+++ script.js
@@ -2,17 +2,18 @@ const vardi = ['Jana Krūmiņa', 'Jānis Bērziņš', 'Dace Priede', 'Atis Roze'
 const balvas = ['Dators', 'Leduskapis', 'Tālrunis', 'Auto', 'Fotoaparāts'];
 const naudaKopa = 100000;//kopeja summa
 let uzvaretajuSkaits = 5;
-//let balvuSkaits = 5;
+let balvuSkaits = 5;
 let rindas = document.querySelector('.rindas');
 rindas.innerHTML = ' ';
 //rindas.innerHTML = ' ';
-for (let i = 0; i < uzvaretajuSkaits;i++) {
+for (let i = 0; i < uzvaretajuSkaits,balvuSkaits;i++) {
     let rand = Math.random() * vardi.length;
     let rand1 = Math.random()*balvas.length;
     rand = Math.floor(rand);//noapaļo uz leju
+    rand1=Math.floor(rand1);
 
     let uzvaretajs = vardi[rand];//izvada konsolē
-    let balvas= balvas[rand1]:
+    let balvas= balvas[rand1];
     
         rindas.innerHTML += `
 <tr>
