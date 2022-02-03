9c06a0ea20780ecc1945ccbcd0e5421fa2c0c1e4
skolotaja <maiga.pigita@kkv.lv>
2022-02-03 14:45:56 +0200

    balv

--- script.js
+++ script.js
@@ -2,23 +2,22 @@ const vardi = ['Jana Krūmiņa', 'Jānis Bērziņš', 'Dace Priede', 'Atis Roze'
 const balvas = ['Dators', 'Leduskapis', 'Tālrunis', 'Auto', 'Fotoaparāts'];
 const naudaKopa = 100000;//kopeja summa
 let uzvaretajuSkaits = 5;
-let balvuSkaits = 5;
 let rindas = document.querySelector('.rindas');
 rindas.innerHTML = ' ';
 //rindas.innerHTML = ' ';
-for (let i = 0; i < uzvaretajuSkaits,balvuSkaits;i++) {
+for (let i = 0; i < uzvaretajuSkaits;i++) {
     let rand = Math.random() * vardi.length;
-    //let rand1 = Math.random()*balvas.length;
     rand = Math.floor(rand);//noapaļo uz leju
-    //rand1=Math.floor(rand1);
+
 
     let uzvaretajs = vardi[rand];//izvada konsolē
-    //let balvas= balvas[rand1];
+    let balvas = balvas[rand]
+    
     
         rindas.innerHTML += `
 <tr>
 <td>${i + 1}</td>
 <td>${uzvaretajs}</td>
-//<td>${balvas}</td>
+<td>${balvas}>/td>
 </tr>`
     }
